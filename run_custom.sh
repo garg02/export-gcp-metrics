6574478536d70dd44b30346e621756cfa8d4c5b3
Raghav Garg <76666461+garg02@users.noreply.github.com>
2022-07-21 15:45:46 +0530

    Delete run_custom.sh

--- run_custom.sh
+++
@@ -1,8 +0,0 @@
-curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-sudo apt-get install -y stress-ng git golang-go
-git clone https://github.com/garg02/gcp_report_batch.git
-cd gcp_report_batch
-go mod init metrics
-go build batch_metrics.go
-chmod +x *.sh
