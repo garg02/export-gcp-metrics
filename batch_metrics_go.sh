9877771d9902268673a2200cf96d19e388f9c018
Raghav Garg <76666461+garg02@users.noreply.github.com>
2022-07-23 11:11:59 +0530

    Update and rename batch_metrics_guppy.sh to batch_metrics_go.sh

---
+++ batch_metrics_go.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /tmp/gce_meta
+source /tmp/curr_time_guppy
+gcp_report_batch/batch_metrics_guppy -batch_gpu=$CURRTIME -instID=$INSTANCE_ID -projID=$PROJECT_ID -zoneID=$ZONE_ID
--- batch_metrics_guppy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /tmp/gce_meta
-source /tmp/curr_time_guppy
-gcp_report_batch/batch_metrics_guppy -batch=$CURRTIME -instID=$INSTANCE_ID -projID=$PROJECT_ID -zoneID=$ZONE_ID
