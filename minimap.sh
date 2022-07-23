3cd6b5a3f9ccc6f66be2f30b40b16410a07c3a5c
Raghav Garg <76666461+garg02@users.noreply.github.com>
2022-07-23 11:53:01 +0530

    Update minimap.sh

--- minimap.sh
+++ minimap.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+echo "export CURRTIME=$(date +%s)" > /tmp/curr_time
 ./minimap2-2.24_x64-linux/minimap2 -ax map-ont --MD -t 14 chr17.fa n_guppy.fq | samtools view -F 0x904 -hb -@6 | samtools sort -@6 | samtools view -hb -@6 > all.bam
 samtools index -@14 all.bam
