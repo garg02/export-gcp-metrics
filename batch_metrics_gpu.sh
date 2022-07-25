6fc2786b2a34602ef8bdd00355d6abbd276311d5
Raghav Garg <76666461+garg02@users.noreply.github.com>
2022-07-26 01:27:55 +0530

    Update batch_metrics_gpu.sh

--- batch_metrics_gpu.sh
+++ batch_metrics_gpu.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /tmp/gce_meta
-gcp_report_batch/batch_metrics_gpu -batch_gpu=$(date +%s) -instID=$INSTANCE_ID -projID=$PROJECT_ID -zoneID=$ZONE_ID
+source /tmp/curr_time_gpu
+gcp_report_batch/batch_metrics_gpu -batch_gpu=$CURRTIME_GPU -instID=$INSTANCE_ID -projID=$PROJECT_ID -zoneID=$ZONE_ID
