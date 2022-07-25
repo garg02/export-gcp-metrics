66d73255f96fc78eb09a3e3e91dedad494849bfb
Raghav Garg <76666461+garg02@users.noreply.github.com>
2022-07-25 19:09:36 +0530

    Update batch_metrics.sh

--- batch_metrics.sh
+++ batch_metrics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /tmp/gce_meta
-gcp_report_batch/batch_metrics -batch=$CURRTIME -instID=$INSTANCE_ID -projID=$PROJECT_ID -zoneID=$ZONE_ID
+gcp_report_batch/batch_metrics -batch=$(date +%s) -instID=$INSTANCE_ID -projID=$PROJECT_ID -zoneID=$ZONE_ID
